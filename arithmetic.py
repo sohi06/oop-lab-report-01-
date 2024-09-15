4636171dc5203b583aee96fe852505502f4de103
sohi06 <168390978+sohi06@users.noreply.github.com>
2024-09-16 00:36:05 +0600

    Initial commit

---
+++ arithmetic.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+print(f"addition ={a+b}")
+print(f"subtraction ={a-b}")
+print(f"multiplication ={a*b}")
+print(f"division ={a/b}")
